103ac7e9d35f32fa7a7bca4648fa765511677a41
Víctor Florence de Quadros <m102139@ACAD.UNISC.BR>
2020-01-14 17:18:28 -0300

    adicao dos menus em .txt

---
+++ src/menus/MenusUtilizados.java
@@ -0,0 +1,39 @@
+package menus;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MenusUtilizados {
+
+	public void Inicial() throws IOException{
+		
+		FileInputStream fis = new FileInputStream("Compasso ART ASCII");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		
+		String linha = br.readLine();
+		
+		while(linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		br.close();
+	}
+	
+	
+	
+	public void Geral(){
+		
+		System.out.println("_____________________________________");
+		System.out.println("## Escolha uma das opçoes a baixo ##");
+		System.out.println("Opcao 1 - Cadastra pessoas");
+		System.out.println("Opcao 2 - Imprime pessoas cadastradas");
+		System.out.println("Opcao 0 - Sair do programa");
+		System.out.println("_____________________________________");
+	}
+
+}
---
+++ src/testes/testaMenuStart.java
@@ -0,0 +1,16 @@
+package testes;
+
+import menus.MenusUtilizados;
+
+public class testaMenuStart {
+
+	public static void main(String[] args) throws Exception {
+		
+	  MenusUtilizados Menu = new MenusUtilizados();
+	  
+	  Menu.Inicial();
+	  Menu.Geral();
+
+	}
+
+}
